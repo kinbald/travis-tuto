5e137f2b8a5e9ae42521b8c90ea218f81afba59e
Kinbald <guillaume.desrumaux@isen.yncrea.fr>
2019-05-14 15:42:34 +0200

    Correct Update

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 
 app.get('/', (req, res) => { // new
-    res.send('Homepage! Hello world.');
+    res.send('Homepage! Hello world. V1');
 });
 
 app.listen(3000, () => console.log('listening on port 3000'));
